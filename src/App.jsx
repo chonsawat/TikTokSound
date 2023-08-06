50fbc13900c85915aa8207b7f245a1cd96e37858
chonsawat <chonsawat.nakanam@kkumail.com>
2023-08-06 10:54:23 +0700

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div className="container">
+      <h1>TikTok Sound!</h1>
+      <p>Play sound rely on event such as Gift, Like, Subcribe.</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a Tiktok ID..."
+        />
+        <button type="submit">Connect</button>
+      </form>
+
+      <p>{greetMsg}</p>
+    </div>
+  );
+}
+
+export default App;
